8da6a077cf863df93a8656630afdf963f3683259
Yaman Ahmed <yamankhanse@gmail.com>
2025-04-18 14:25:21 +0500

    final changes

--- src/index.js
+++ src/index.js
@@ -120,5 +120,5 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`http://localhost:${port}`);
 });
